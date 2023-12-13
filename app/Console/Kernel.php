857a5ac3d68c044d7de7c2c7897cf4d0c9deb823
HectorSauR <hector.sauceda.01@gmail.com>
2023-12-13 00:33:15 -0700

    check loans cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,17 +2,21 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CheckLoans;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        CheckLoans::class,
+    ];
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('check:loans')->daily();
     }
 
     /**
